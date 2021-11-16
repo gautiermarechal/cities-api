d76bffb258674962a4a8a6a71fc6da594a3507f8
Gautier Marechal <gaut.marechal@gmail.com>
2021-11-16 08:41:19 -0500

    Fix

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ app.use(express.json());
 
 const port = process.env.PORT || 4000;
 
-if (!process.env.PORT) {
-  app.listen(port, function () {
-    console.log(`Server listening on port ${port}`);
-  });
-}
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}`);
+});
 
 app.get("/", (req, res) => {
   res.json({ message: `Server is running on port ${port}` });
